556162bc1168bec386f191b6052ac6efaf94fe32
Biljana Kotevska <biljanakotev@gmail.com>
2023-09-23 18:45:17 +0200

    finished iteration 2

--- routes/beers.routes.js
+++ routes/beers.routes.js
@@ -2,18 +2,44 @@ const router = require('express').Router();
 const Beer = require('../models/Beer.models');
 
 // POST
-router.post('/beers', (req, res) => {
-  // deconstructing the beer model object that travels in the body
 
+// deconstructing the beer model object that travels in the body
+
+router.post('/beers', (req, res) => {
   const { image_url, name, tagline, first_brewed, description, attenuation_level, brewers_tips } = req.body;
 
   Beer.create({ image_url, name, tagline, first_brewed, description, attenuation_level, brewers_tips })
     .then((newBeer) => {
-      res.json({ newBeer });
+      res.json(newBeer);
     })
     .catch((err) => {
       res.json(err);
     });
 });
 
+// no need to deconstuct when you want to send the whole array
+
+// router.post('/beers', (req, res) => {
+//   const beersArray = req.body;
+
+//   Beer.insertMany(beersArray)
+//     .then((newBeers) => {
+//       res.json(newBeers);
+//     })
+//     .catch((err) => {
+//       res.json(newBeers);
+//     });
+// });
+
+// GET
+
+router.get('/beers', (req, res) => {
+  Beer.find()
+    .then((allBeers) => {
+      res.json(allBeers);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
