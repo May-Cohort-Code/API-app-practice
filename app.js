51642ae298da862c44393d8cf5562f4fc415c6e0
Biljana Kotevska <biljanakotev@gmail.com>
2023-09-23 15:39:57 +0200

    added readme

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
-app.use("/api", require('./routes/beer.routes'));
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
